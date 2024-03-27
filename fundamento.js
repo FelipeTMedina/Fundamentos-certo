38a4dbd006ec9486f6a22dab56a1336b982f60d0
thiagoBventura <thiagomutuca@gmail.com>
2024-03-26 22:00:13 -0300

    feat: add fundamentos

---
+++ fundamento.js
@@ -0,0 +1,33 @@
+console
+
+//tipos de dados
+
+const name = "Felipe Teles Medina"
+let qtd =5
+const isAproved = false
+let lastName = null
+let age
+const languages = ["javaScript", "PHP", "Python"];
+const user = {email: "lcs@teste.com", passoword: "teste123"};
+
+
+console.log(name);
+console.log(qtd);
+console.log(isAproved);
+console.log(lastName);
+console.log(age);
+console.log(languages);
+console.log(user);
+
+let obj =[
+{ name: "felipe", age: 25},
+{ name: "luciana", age: 45},
+{ name: "luana",  age: 45},
+{ name: "medina", age: 57},
+]
+
+let num = [1,2,3,4,5]
+
+
+
+console.log(obj, num);
