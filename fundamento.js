5acdac03d2d4f06b81a61e7acd90fecff667f09f
thiagoBventura <thiagomutuca@gmail.com>
2024-03-27 22:05:51 -0300

    feat:_add_new_feats

--- fundamento.js
+++ fundamento.js
@@ -5,7 +5,7 @@ console
 const name = "Felipe Teles Medina"
 let qtd =5
 const isAproved = false
-let lastName = null
+let lastNamee = null
 let age
 const languages = ["javaScript", "PHP", "Python"];
 const user = {email: "lcs@teste.com", passoword: "teste123"};
@@ -14,16 +14,17 @@ const user = {email: "lcs@teste.com", passoword: "teste123"};
 console.log(name);
 console.log(qtd);
 console.log(isAproved);
-console.log(lastName);
+console.log(lastNamee);
 console.log(age);
 console.log(languages);
 console.log(user);
 
 let obj =[
-{ name: "felipe", age: 25},
-{ name: "luciana", age: 45},
-{ name: "luana",  age: 45},
-{ name: "medina", age: 57},
+{nome: "Felipe", age: 25},
+{nome: "Matheus",age: 30},
+{nome: "luciana",age: 20},
+{nome: "medina", age: 14},
+
 ]
 
 let num = [1,2,3,4,5]
@@ -31,3 +32,46 @@ let num = [1,2,3,4,5]
 
 
 console.log(obj, num);
+
+
+
+//método de string é algo que posso fazer a partir de uma variavel. pesquisar sobre ESLINT/ Prettier
+
+const fullName ="FelipexTelesxMedina"
+
+console.log(fullName, "nome completo");
+
+const stringToArray = fullName.split("x")
+
+console.log(stringToArray);
+
+const primeiroNome = stringToArray[0]
+const ultimoNome = stringToArray [2]
+
+console.log( "Bem vindo",primeiroNome,ultimoNome);
+
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+console.log(fullName.length)
+console.log(fullName.indexOf("Medina"))
+console.log(fullName.slice(0,6))
+
+
+
+//métodos de array
+
+const list = ['a','b','c',]
+
+console.log(list.length, 'array')
+console.log(list[0])
+list[3] ="f"
+console.log(list)
+
+list.push('g')
+
+console.log(list,"push")
+
+console.log(list[list.length-1], "aqui")
+
+list.pop()
+console.log (list)
